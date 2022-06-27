bea3228cf0eb2f87085535a154356eab9f32a7ba
Andrew Metallinos <andrew@metallinostech.com.au>
2022-06-27 21:24:18 +1000

    Update System Information Grabber v1.7.0.ps1

--- System Information Grabber v1.7.0.ps1
+++ System Information Grabber v1.7.0.ps1
@@ -64,6 +64,23 @@ Write-Host "
 
 
 
+#Open script in Administrator mode
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
+
+
 Write-Host "The users/domain/workgroup details are below:"
 Get-ComputerInfo | Format-List -Property @{n="*Owner";e={$_.WindowsRegisteredOwner}},
                                          @{n="Username";e={$_.CsUserName}},
